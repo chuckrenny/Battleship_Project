a94811b7017cec1e6c811ff1bb8fadc0094849f3
Charles Ren <cren04@syr.edu>
2023-07-23 12:36:52 -0700

    Merge Conflict Resolution

--- lib/board.rb
+++ lib/board.rb
@@ -28,7 +28,8 @@ class Board
 
     def valid_placement?(ship, coordinate_array)
       if coordinate_array.length == ship.length
-        if horizontal_placement(coordinate_array) || vertical_placement(coordinate_array)
+        if helper_placement(coordinate_array) && 
+        coordinate_array.all? {|cell| @cells[cell].empty?}
           true
         else 
           false
@@ -43,25 +44,62 @@ class Board
     numbers = coordinate_array.map { |coordinate| coordinate[1].to_i} 
 
     # pull out all the letters, need to be all the same
-    letters = coordinate_array.map { |coordinate| 
-      coordinate[0] 
-    } #['A', 'A', 'A']
+    letters = coordinate_array.map { |coordinate| coordinate[0] } 
 
-    # numbers are increasing order && letters all the|  same
-    letters.uniq.length == 1 && numbers == (numbers.first..numbers.last).to_a 
-    #another array of numbers but in consecutive order
+    # horizontal: numbers are increasing order && letters all the same
+    (letters.uniq.length == 1 && numbers == (numbers.first..numbers.last).to_a) || 
+    # vertical: numbers are all the same && letters are in consecutive increasing order
+    (numbers.uniq.length == 1 && ("A".."D").each_cons(coordinate_array.count).any? {|each| letters == each})
   end
 
-  def vertical_placement(coordinate_array)
-    numbers = coordinate_array.map { |coordinate|
-      coordinate[1].to_i
-    }
+  def place(ship, coordinate_array)
+    if valid_placement?(ship, coordinate_array)
+      coordinate_array.each do |cell|
+        @cells[cell].place_ship(ship) 
+      end
+    end
+  end
 
-    letters = coordinate_array.map { |coordinate| 
-      coordinate[0] 
-    }
+  def render(player = false)
+    # The # x # size of the board
+    board_size = 4
+    # Creates an array for all letters
+    alphabet = ("A".."Z").to_a
+    # Creates the numbers array that will bereferenced later. Dynamic based on board size.
+    nums = (1..board_size).to_a
+    # Creates the letters array that will be referenced later. Dynamic as well.
+    letts = ("A"..alphabet[board_size - 1]).to_a
+    # Creates arrays of every combo of nums and letts. 
+    places = letts.product(nums) 
 
-    # numbers are all the same && letters are in consecutive increasing order
-    numbers.uniq.length == 1 && ("A".."D").each_cons(coordinate_array.count).find {|each| letters == each}
+    board = 
+    # Starting a game array which will hold the rows of the board
+      game = []
+      # For each letter create a row array
+      letts.map do |lett|
+        row = []
+        # Once looking at a single letter, look at the places array for every matching letter.
+        places.each do |place|
+          # When the first element matches letter, join it with it's number and shovel into row.
+          if place[0] == lett
+            row << place.join
+          end
+        end
+        # Once all of a certain letter has been placed into a row, shovel that into game.
+        game << row
+      end
+      game
+
+    # final projection
+    projection = ["  #{nums.join(" ")} \n"]
+    board.each_with_index do |row, index|
+      new_row = row.map do |cell|
+        @cells[cell].render(player)
+      end
+
+      projection.push("#{letts[index]} " + new_row.join(" ") + " \n")
+    end
+
+    projection.join("")
   end
 end
