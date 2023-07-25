c17b6fb686675ffd68915c90443e5a4749448d8c
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-25 13:14:38 -0700

    refactor: removing pseudo code/ docs

--- lib/setup.rb
+++ lib/setup.rb
@@ -26,17 +26,6 @@ class Setup
     @player.place(ship, coordinate_array)
   end
  
-  # def main_menu
-  #   puts "Welcome to BATTLESHIP!" 
-  #   puts "Enter p to play. Enter q to quit."
-  #   input = gets.chomp
-  #   if input == "q"
-  #     return puts "See you later!"
-  #   else
-  #   begin_game(input)
-  #   end
-  # end
-
   def main_menu
     loop do
       puts "Welcome to BATTLESHIP!" 
@@ -47,8 +36,6 @@ class Setup
         begin_game
       elsif input == "q"
         puts "See you later!"
-
-        # added, prevents any further code from running
         exit(0)
       else
         puts "Invalid input, please try again."
@@ -58,33 +45,26 @@ class Setup
 
   def begin_game
       puts "Let's Play!"
-      # setup = Setup.new
       cruiser = Ship.new("Cruiser", 3)
       submarine = Ship.new("Submarine", 2)
-      # setup.computer_placement(cruiser)
       computer_placement(cruiser)
-      # setup.computer_placement(submarine)
       computer_placement(submarine)
     
-      # puts setup.computer.render(true)
       puts @computer.render(true)
       puts "I have laid out my ships on the grid. \n" +
             "You now need to lay out your ships \n" +
             "The Cruiser is three units long and the Submarine is two unit long"
-      # puts setup.player.render
       puts @player.render
       puts " Enter the squares for the Cruiser (3 spaces):  \n" +
            " Example: A1, A2, A3"
       cruiser1_placement = gets.chomp
       cruiser1 = Ship.new("Cruiser", 3)
     
-      # while(setup.player_placement(cruiser1, cruiser1_placement) == false)
       while(player_placement(cruiser1, cruiser1_placement) == false)
         puts "Those are invalid cruiser coordinates. Please try again:"
         cruiser1_placement = gets.chomp
       end
       
-      # puts setup.player.render(true)
       puts @player.render(true)
     
       puts " Enter the squares for the Submarine (2 spaces):  \n" +
@@ -92,15 +72,12 @@ class Setup
       submarine1_placement = gets.chomp
       submarine1 = Ship.new("Submarine", 2)
     
-      # while(setup.player_placement(submarine1, submarine1_placement) == false)
       while(player_placement(submarine1, submarine1_placement) == false)
         puts "Those are invalid submarine coordinates. Please try again:"
         submarine1_placement = gets.chomp
       end
-      # puts setup.player.render(true)
       puts @player.render(true)
       
-      #setup.run
       run
   end
 
