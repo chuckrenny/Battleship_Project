2d6bac34261abca44801180cd82c980643f2fc6a
Charles Ren <cren04@syr.edu>
2023-07-26 13:49:26 -0700

    removed docs and previous refactor

--- lib/setup.rb
+++ lib/setup.rb
@@ -214,10 +214,7 @@ puts "_________/\\\\\\\\\\\\\\\\\\\\\\\\\\_______/\\\\\\\\\\\\\\\\\\_____/\\\\\\
       end
       computer.cells[player_shot].fire_upon
 
-      #initial random computer shot 
       sample_computer_shot = player.cells.keys.sample 
-
-      # if intelligent_shot array is not empty, sample shot will be the first intelligent array value
       sample_computer_shot = intelligent_shot.shift if !intelligent_shot.empty?  
       
       while player.cells[sample_computer_shot].fired_upon? 
@@ -238,8 +235,6 @@ puts "_________/\\\\\\\\\\\\\\\\\\\\\\\\\\_______/\\\\\\\\\\\\\\\\\\_____/\\\\\\
         puts "My shot on #{player.cells[sample_computer_shot].coordinate} was a miss."
       elsif player.cells[sample_computer_shot].render == 'H'
         puts "My shot on #{player.cells[sample_computer_shot].coordinate} was a HIT!"
-        # added
-        # select for adjacent shots that have not been fired_upon
         intelligent_shot.concat(player.adjacent_cells(sample_computer_shot))
       elsif player.cells[sample_computer_shot].render == 'X'
         puts "My shot on #{player.cells[sample_computer_shot].coordinate} sunk your #{player.cells[sample_computer_shot].ship.name}"
